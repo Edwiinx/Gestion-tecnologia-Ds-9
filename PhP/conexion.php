21566b489820b3c350e8f7583e2251e3a354137a
Edwiinx <edwin.mina@utp.ac.pa>
2025-04-23 16:13:40 -0500

    HSBHDFBKJBVJK

--- PhP/conexion.php
+++ PhP/conexion.php
@@ -2,12 +2,12 @@
   
   
   // Conexion a la base de datos
-  $host = "localhost";
-  $username = "mario";
-  $password = "12345678";
-  $dbname = "noid_bd";
+$host = "localhost";
+$username = "mario";
+$password = "12345678";
+$dbname = "noid_bd";
   
-  $conn = mysqli_connect($host, $username, $password, $dbname);
+$conn = mysqli_connect($host, $username, $password, $dbname);
    
 $host = "localhost";
 $user = "root";
